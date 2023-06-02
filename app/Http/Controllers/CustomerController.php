65b79dbcab690aa4bed3218b9331c5d7347e0572
eneskaracaa0 <eneskaraca18.ek@gmail.com>
2023-06-03 00:22:06 +0300

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+
+    {
+       $customers=Customer::all();
+    return view('customers.index')->with('customers',$customers);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('customers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCustomerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        Customer::create($request->validated());
+        return back()->with('success','Customer Added Succesfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return view('customers.show')->with('customer',$customer);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+         return view('customers.edit')->with('customer',$customer);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCustomerRequest  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+
+        $customer->update($request->validated());
+        return back()->with('success','Customer Edited Succesfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return back()->with('success','Customer Deleted Succesfully');
+    }
+}
---
+++ resources/views/customers/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="card col-6 offset-3">
+  <h5 class="card-header">Add Customer</h5>
+  <div class="card-body">
+      @include('messages')
+    <form action="/customers" method="POST">
+        @csrf
+        <div class="form-group">
+            <label class="form-label">Name</label>
+            <input type="text" name="name"  class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
+            @error('name')
+            <span class="text-danger">
+                <strong>{{$message}}</strong>
+            </span>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label class="form-label">Email</label>
+            <input type="email" name="email" class="form-control" class="form-control @error('email') is-invalid @enderror" value="{{old('email')}}">
+            @error('email')
+            <span class="text-danger">
+                <strong>{{$message}}</strong>
+            </span>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label class="form-label">Phone</label>
+            <input type="text" name="phone" class="form-control" class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}">
+            @error('phone')
+            <span class="text-danger">
+                <strong>{{$message}}</strong>
+            </span>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <button class="btn btn-info" type="submit">Submit</button>
+        </div>
+    </form>
+
+  </div>
+</div>
+</div>
+
+@endsection
